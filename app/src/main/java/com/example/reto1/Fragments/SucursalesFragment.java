b179b1f001de25049e52237667937df2d13f0c4a
NELL_OLAYA <yesid.olaya@gmail.com>
2023-08-12 09:55:11 -0500

    Reto4 agregando mapa

--- app/src/main/java/com/example/reto1/Fragments/SucursalesFragment.java
+++ app/src/main/java/com/example/reto1/Fragments/SucursalesFragment.java
@@ -9,9 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.reto1.R;
 
-public class SucursalesFragment extends Fragment {
+import com.example.reto1.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SucursalesFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
 
     public SucursalesFragment(){
@@ -27,12 +36,41 @@ public class SucursalesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.sucursales_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapContainer);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.mapContainer, mapFragment)
+                    .commit();
+            mapFragment.getMapAsync(this);
+        }
     }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        // Add a marker in Bogotá and set a closer zoom level
+        LatLng bogota = new LatLng(4.750340, -74.119895);
+        float zoomLevel = 12f; // Ajusta el valor para lograr el nivel de zoom deseado
+        mMap.addMarker(new MarkerOptions().position(bogota).title("Sede Suba"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bogota, zoomLevel));
+
+        // Ubicación sede chapinero  4.650493669603073, -74.06133722556912
+        LatLng Chapinero = new LatLng(4.650493669603073, -74.06133722556912); // Reemplaza con las coordenadas de la otra ciudad
+        mMap.addMarker(new MarkerOptions().position(Chapinero).title("Sede Chapinero"));
+        // Ubicación sede Fontibon 4.677054, -74.142900
+        LatLng Fontibon = new LatLng(4.677054, -74.142900); // Reemplaza con las coordenadas de la otra ciudad
+        mMap.addMarker(new MarkerOptions().position(Fontibon).title("Sede Fontibon"));
+    }
+
 }
